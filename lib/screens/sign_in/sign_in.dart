453c91acf0bf78e765159c8c3a940173192579d1
Coelho <lcsilva2099@gmail.com>
2022-09-30 13:09:35 -0300

    finished authentication

--- lib/screens/sign_in/sign_in.dart
+++ lib/screens/sign_in/sign_in.dart
@@ -1,15 +1,72 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:zip_zopperson/bloc/auth_bloc/auth_bloc.dart';
+import 'package:zip_zopperson/screens/messages/messages.dart';
 
 class SignIn extends StatelessWidget {
   const SignIn({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: BlocListener<AuthBloc, AuthState>(
+        listener: (context, state) {
+          if (state is Authenticated) {
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(builder: (context) => const Messages()),
+            );
+          }
+          if (state is AuthError) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text(state.error)),
+            );
+          }
+        },
+        child: BlocBuilder<AuthBloc, AuthState>(
+          builder: (context, state) {
+            if (state is Loading) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            if (state is UnAuthenticated) {
+              return Container(
+                alignment: Alignment.center,
+                child: ElevatedButton(
+                  onPressed: () {
+                    _authenticateWithGoogle(context);
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 10.0, 20.0, 10.0),
+                        child: Image.asset(
+                          'assets/images/google-logo-9808.png',
+                          height: 40.0,
+                        ),
+                      ),
+                      const Text(
+                        'Continue with Google',
+                        style: TextStyle(color: Colors.black),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            }
+            return Container();
+          },
+        ),
+      ),
+    );
   }
 
-  void _authenticatedWithGoogle(BuildContext context) =>
+  void _authenticateWithGoogle(BuildContext context) =>
       BlocProvider.of<AuthBloc>(context).add(GoogleSignInRequestedEvent());
 }
